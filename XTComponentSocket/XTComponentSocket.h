13f31949c1993260470b0795c3943d8a3f87722a
351706354@qq.com <351706354@qq.com>
2018-11-08 16:02:37 +0800

    Initial Commit

---
+++ XTComponentSocket/XTComponentSocket.h
@@ -0,0 +1,19 @@
+//
+//  XTComponentSocket.h
+//  XTComponentSocket
+//
+//  Created by apple on 2018/11/8.
+//  Copyright © 2018年 新天科技股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XTComponentSocket.
+FOUNDATION_EXPORT double XTComponentSocketVersionNumber;
+
+//! Project version string for XTComponentSocket.
+FOUNDATION_EXPORT const unsigned char XTComponentSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XTComponentSocket/PublicHeader.h>
+
+
